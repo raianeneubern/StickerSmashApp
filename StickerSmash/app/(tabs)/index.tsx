3048386829ea5cd4441abfada4fae252f4729f8a
raianeneubern <raianeneubern@gmail.com>
2025-06-02 07:42:35 -0300

    atualizado para funcionar no navegador

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
+import { ImageSourcePropType, View, StyleSheet, Platform } from "react-native";
 import { useState, useRef } from "react";
 import { captureRef } from 'react-native-view-shot';
-import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
@@ -12,6 +12,7 @@ import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import * as MediaLibrary from 'expo-media-library';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
@@ -39,18 +40,35 @@ export default function Index() {
     SetIsModalVisible(false);
   }
 
-  const onSaveImageAsync = async() => {
-    try {
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
         const localUri = await captureRef(imageRef, {
-          height: 440, 
+          height: 440,
           quality: 1,
         });
         await MediaLibrary.saveToLibraryAsync(localUri);
         if (localUri) {
           alert('Imagem salva com sucesso')
         }
-    } catch (e) {
-      console.log(e)
+      } catch (e) {
+        console.log(e)
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 
